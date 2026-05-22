a176cfd6b0ab640eac62afb993149d8485ae59e7
Mimis <dim_kar@outlook.com>
2026-05-22 23:44:48 +0300

    add version in Info section

--- TANREN-Metsuke/Views/InfoView.axaml.cs
+++ TANREN-Metsuke/Views/InfoView.axaml.cs
@@ -1,8 +1,14 @@
+using System.Reflection;
 using Avalonia.Controls;
 
 namespace TANREN_Metsuke.Views;
 
 public partial class InfoView : UserControl
 {
-    public InfoView() => InitializeComponent();
+    public InfoView()
+    {
+        InitializeComponent();
+        var version = typeof(InfoView).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "?";
+        CopyrightText.Text = $"© 2026 Dimitris Karatzas | TANREN Metsuke v{version}";
+    }
 }
